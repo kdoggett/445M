23129a3198d485e63c2e6681dfab08f741bbd1fd
kdoggett <kdoggett@gmail.com>
2015-03-31 14:08:37 -0500

    no message

---
+++ Lab 4 - Digital Scope and Spectrum Analyzer/MACQ.h
@@ -0,0 +1,18 @@
+#include <stdint.h>
+
+/***** MACQ_INIT ****
+* INITIALIZE POINTERS OF MACQ *
+*/
+void MACQ_Init (void);
+
+
+/**** MACQ_PUT ******
+* MUCH LIKE FIFO PUT BUT WRAPS AROUND *
+*/
+void MACQ_Put (int32_t MACQ_DATA);
+
+
+/**** MACQ_GET ******
+* MUCH LIKE FIFO GET BUT WRAPS AROUND *
+*/
+int32_t MACQ_Get (void);
